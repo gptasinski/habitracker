0de440439565bf7bc44c810e8fe7caf49a919e53
gptasinski <g.ptasinski78@gmail.com>
2016-06-10 14:08:59 -0500

    Add best/worst times functionality and display to SwimStats

--- app/helpers/swim_stat_helper.rb
+++ app/helpers/swim_stat_helper.rb
@@ -5,5 +5,28 @@ module SwimStatHelper
     @user.swim_stats.order(created_at: "ASC")
   end
 
+  def targets(distance)
+    user = User.find(session[:id])
+    swimstats = user.swim_stats
+    swimstats.pluck(distance)
+  end
+
+  def reject_zeros(times)
+    times.reject { |time| time == "0" || time == nil }
+  end
+
+  def best_time(distance)
+    reject_zeros(targets(distance)).min
+  end
+
+  def worst_time(distance)
+    reject_zeros(targets(distance)).max
+  end
+
+  def bw_date(distance, best_time)
+    swimstat = SwimStat.find_by("#{distance}": best_time)
+    swimstat == nil ? "" : swimstat.date
+  end
+
 
 end
