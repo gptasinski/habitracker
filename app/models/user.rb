0edb166d8b39920f7e4ec3bf89d6da3d0ee51ddf
gptasinski <g.ptasinski78@gmail.com>
2016-09-10 13:56:37 -0500

    Add borders back to totals on index views. Add minor validations for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-
   has_many :smoker_stats
   has_many :swim_stats
+
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
+
+
 
 end
